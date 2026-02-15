5a91d397a51a59999c9736f1b6fd6aa690e10366
sures <sureshassuri1122@gmail.com>
2026-02-15 17:21:45 +0530

    commited by asian timezone guy

--- src/Interview.java
+++ src/Interview.java
@@ -5,22 +5,10 @@ import java.util.Map.Entry;
 public class Interview {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a = {1,1,2,2,2,4,4,4,4,4};
-		
-		Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-		for(int t : a) {
-			map.put(t, map.getOrDefault(t, 0)+1);
-		}
-//		System.out.println(map);
-		
-		for(Entry<Integer, Integer> m : map.entrySet()) {
-//			System.out.println(m.get);
-		}
-		
-		int aa=10;
-		Integer ww = aa;
-		aa=ww;
+
+		System.out.println("postjira");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
 	}
 
 }
