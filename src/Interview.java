bb983509ed2194ab9cf0eef317f98a8195e91b9d
sures <sureshassuri1122@gmail.com>
2026-02-16 10:35:45 +0530

    src no bin

--- src/Interview.java
+++ src/Interview.java
@@ -7,6 +7,8 @@ public class Interview {
 	public static void main(String[] args) {
 
 		System.out.println("postjira");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
 	
 	}
 
